4b24d9f0164aee71fa6a4a311afcb913b90b47c4
ubisam-heung <heung@ubisam.com>
2026-02-26 13:50:47 +0900

    20260226-global 수정

--- src/main/java/com/github/ubisam_heung/api_response/GlobalExceptionHandler.java
+++ src/main/java/com/github/ubisam_heung/api_response/GlobalExceptionHandler.java
@@ -3,14 +3,14 @@ package com.github.ubisam_heung.api_response;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // 기본 예외 처리
